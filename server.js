6ae804a28a67baafd9bece3dfacdfb5cc517a8db
comlurb-developer <comlurbdesenvolvimento@gmail.com>
2022-08-29 10:11:40 -0300

    1234

--- server.js
+++ server.js
@@ -6,10 +6,27 @@ app.use(express.json());
 app.use(express.static('express'));
 // URL padrão do site
 app.use('/', (req, res) => {
-    res.sendFile(path.join(__dirname+'/src/index.html'));
+    res.sendFile(path.join(__dirname + '/src/index.html'));
 });
 const server = http.createServer(app);
 var port = normalizePort(process.env.PORT || '3000');
 
 server.listen(port);
 console.debug('Server inicializado na porta ' + port);
+
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
